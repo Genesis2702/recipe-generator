0b6836c54df6db2edc2ec861a8a93102146d4e75
Prime_Genesis <101158478+Genesis2702@users.noreply.github.com>
2025-08-09 17:19:02 +0700

    Fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,7 @@
-import icon from './icon.png'
-
 export default function Header() {
   return (
     <header className="page-header">
-      <img src={icon} alt="chef-icon" className="page-icon"></img>
+      <img src="./icon.png" alt="chef-icon" className="page-icon"></img>
       <span className="page-name">Recipe Generator</span>
     </header>
   )
